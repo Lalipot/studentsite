e864673b7e861532503709cf27dcd0fa88046aba
Ferrllyn Alipot <ficmat@hawaii.edu>
2023-04-06 17:56:56 -1000

    added toggle nav link

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //JAVASCRIPT FUNCTIONS GO HERE  
-    // console.log('JS IS READY');
+    //TOGGLE NAV MOBILE MEU FOR SMALL SCREENS
+    const menubutton = document.querySelector('.menu-button'); 
+    const menunav = document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function () {
+        console.log("hello");
+    
+        if (menunav.getAttribute('data-navstate') === 'open' ) {
+            // if true do this
+            menunav.setAttribute('data-navstate', 'closed');
+        }
+        else {
+            //else do this
+            menunav.setAttribute('data-navstate', 'open')
+        };
+    
+    
+    });
+    //console.log(menubutton);
+    //console.log(menunav);
+
+    // console.log('JS IS READY YES!');
 
 
 });  
