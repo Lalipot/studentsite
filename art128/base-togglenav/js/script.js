6368d7de9d907e2e4025a5d6b33f4cc806b1c5a6
Ferrllyn Alipot <ficmat@hawaii.edu>
2023-04-13 07:46:05 -1000

    updated JS

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -1,26 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    //TOGGLE NAV MOBILE MEU FOR SMALL SCREENS
-    const menubutton = document.querySelector('.menu-button'); 
-    const menunav = document.querySelector('.toggle-nav');
-    menubutton.addEventListener('click', function () {
-        console.log("hello");
-    
-        if (menunav.getAttribute('data-navstate') === 'open' ) {
-            // if true do this
-            menunav.setAttribute('data-navstate', 'closed');
-        }
-        else {
-            //else do this
-            menunav.setAttribute('data-navstate', 'open')
-        };
-    
-    
+document.addEventListener("DOMContentLoaded", function() {
+  
+    //TOGGLE MENU NAV VIA ADDING/REMOVING A DATA-ATTRIBUTE
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav'); 
+    menubutton.addEventListener('click', function() {
+      if (menunav.getAttribute('data-navstate') === 'open') {
+          menunav.setAttribute('data-navstate', 'closed');
+      } else {
+          menunav.setAttribute('data-navstate', 'open');
+      }
     });
+  
+  });
     //console.log(menubutton);
     //console.log(menunav);
 
     // console.log('JS IS READY YES!');
 
 
-});  
